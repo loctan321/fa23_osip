90d748db6c4c1ed1c8b7f14c5a291040a5705304
cyrus79-unlimit <giahuy12377@gmail.com>
2023-10-17 14:53:54 +0700

    Revert "modified login screen and add register screen"
    This reverts commit f8b4c0fb698d069ecfdfc9a18a9e0b3732935311.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:optimizing_stock_investment_portfolio/screens/register_screen.dart';
 import 'package:optimizing_stock_investment_portfolio/screens/widgets/input_widget.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,70 +9,46 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailController = TextEditingController();
 
+  final TextEditingController _emailController = TextEditingController();
+  
   final TextEditingController _passwordController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Text('Login Page'),
-          const SizedBox(
-            height: 30,
-          ),
-          InputWidget(
-            hintText: 'Email',
-            obscureTex: false,
-            controller: _emailController,
-            obscureText: false,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          InputWidget(
-            hintText: 'Password',
-            obscureTex: true,
-            controller: _passwordController,
-            obscureText: true,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Login'),
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                padding: const EdgeInsets.symmetric(horizontal: 50)),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Don't have an account yet? "),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegisterScreen()),
-                  );
-                },
-                child: const Text(
-                  'Sign up',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ]),
+    return  Scaffold(
+     body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        
+        const Text('Login Page'),
+        const SizedBox(height: 30,),
+        InputWidget(
+        hintText: 'Email',
+        obscureTex: false,
+        controller: _emailController,
       ),
+       const SizedBox(height: 30,),
+        InputWidget(
+          hintText: 'Password',
+          obscureTex: true,
+          controller: _passwordController,
+          
+
+        ),
+        const SizedBox(height: 30,),
+        ElevatedButton(
+          onPressed: (){},
+           child: const Text('Login'),
+           style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            padding: const EdgeInsets.symmetric(horizontal: 50)
+           ),)
+
+      ]),
+     ),
     );
   }
 }
