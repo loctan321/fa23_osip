1bd63dee1881074330f28124178a0d5a1205a2ca
loctan321 <locvuong35@gmail.com>
2023-12-13 22:31:50 +0700

    UI user profile

--- lib/api/exception/api_endpoints.dart
+++ lib/api/exception/api_endpoints.dart
@@ -1,9 +1,5 @@
 const baseUrl =
-<<<<<<< HEAD
-    "https://a9ec-2402-800-6370-5623-693a-c6d9-2b1f-d8a5.ngrok-free.app/";
-=======
-    "https://5321-2402-800-6370-5623-212e-b6d2-a907-d934.ngrok-free.app";
->>>>>>> locfuho
+    "https://5321-2402-800-6370-5623-212e-b6d2-a907-d934.ngrok-free.app/";
 
 class UsersApi {
   static const login = "/Users/Login";
